e8a355e0116ac35ba19adcb5e3bf940fdeb0d92d
jihemeki <ncluuititiu@gmail.com>
2017-06-21 18:33:44 +0700

    [DEL] ham display

--- Thesis/sudokuS.py
+++ Thesis/sudokuS.py
@@ -66,15 +66,6 @@ def eliminate(values, s, d):
                 return False
     return values
 
-def display(values):
-    "Display these values as a 2-D grid."
-    width = 1+max(len(values[s]) for s in squares)
-    line = '+'.join(['-'*(width*3)]*3)
-    for r in rows:
-        print ''.join(values[r+c].center(width)+('|' if c in '36' else '')
-                      for c in cols)
-        if r in 'CF': print line
-    print
 
 
 def solve(grid): return search(parse_grid(grid))
